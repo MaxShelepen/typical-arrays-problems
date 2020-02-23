416b7ac371613193ce4363e3d6d30466b43c549e
maxshelepen <maxshelepen25@gmail.com>
2020-02-23 16:54:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0 ) {
+    return 0
+  } 
+  return array.sort((a,b) => a > b ? 1 : -1)[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0 ) {
+    return 0
+  } 
+  return array.sort((a,b) => a > b ? 1 : -1)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0 ) {
+    return 0
+  } 
+ return array.reduce((acc, el) => acc + el,0)/array.length;
 }
